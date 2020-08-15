d9d15a5cb3f3e62260a529d5f74a872aa82428da
silviacfreitas <silvia.freitas@gmail.com>
2020-08-15 01:17:10 -0300

    Atualização Form Field

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`JOSON Server is running in ${port}`);
+  console.log(`JSON Server is running in ${port}`);
 });
