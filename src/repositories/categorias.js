08cbce76c8ecc45b0497f24d82691afaac99c249
silviacfreitas <silvia.freitas@gmail.com>
2020-08-15 00:07:27 -0300

    atualização dados cateforias

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 import config from '../config';
 
-const URL_CATEGORIES = `${config.URL_DATA}/categorias`;
+const URL_CATEGORIES = `${config.URL_DATA}`;
 
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
